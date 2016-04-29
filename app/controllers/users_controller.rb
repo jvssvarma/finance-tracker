0fb84e35fe9b3f8e27401bd06cb62a6c9e835607
jvssvarma <varma.santosh@outlook.com>
2016-04-29 00:22:03 -0700

    friends search added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,27 @@ class UsersController < ApplicationController
 	end
 
 	def my_friends
+		@friendships = current_user.friends
+	end
 
+	def search
+		@users = User.search(params[:search_param])
+		if @users
+			@users = current_user.except_current_user(@users)
+			render partial 'friends/lookup'
+		else
+			render status: :not_found, nothing: true
+		end
+	end
+
+	def add_friend
+		@friend = User.find(params[:friend])
+		current_user.friendships.build(friend_id: @friend_id)
+		if current_user.save
+			redirect_to my_friends_path, notice: "#{friend.full_name} successfully added to your friends"
+		else
+			redirect_to my_friends_path, flash[:error] = "Error adding #{friend.full_name} as friend"
+		end
 	end
 
 end
